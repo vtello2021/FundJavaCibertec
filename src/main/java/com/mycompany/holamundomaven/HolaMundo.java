470aacf7d6bcaa5841d942caea3b1825035c0237
Adminfoco <Adminfoco@10.0.0.5>
2020-12-21 22:14:26 -0500

    prueba de Commit2

--- src/main/java/com/mycompany/holamundomaven/HolaMundo.java
+++ src/main/java/com/mycompany/holamundomaven/HolaMundo.java
@@ -3,6 +3,6 @@ package com.mycompany.holamundomaven;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo...");
+        System.out.println("Hola Mundo!!!");
     }
 }
